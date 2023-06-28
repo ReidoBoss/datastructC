3ff2eb17e3004dbf472c96d837f0cb13ba12151c
ReidoBoss <bryansagarino222@gmail.com>
2023-06-28 22:28:18 +0800

    my own implementation of deleteEnd node

--- testingan.c
+++ testingan.c
@@ -1,36 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node
+typedef struct  Node
 {
-    int x;
+    int x; 
     struct Node* next;
-
 }Node;
 
-void insertEnd(Node** root, int value){
-    Node* new_node = malloc(sizeof(Node));
-    new_node->x = value;
-    new_node ->next = NULL;
-
-    
-    if(*root==NULL){
-        *root = new_node;
-        return;
-    }
-
-    Node* curr = *root;
-    while(curr->next!=NULL){
-        curr = curr->next;
-    }
-
-    curr->next = new_node;
-    
-
-
-}
-
-void display(Node* root){
+void display (Node* root){
 
     if (root==NULL)
     {
@@ -39,14 +16,39 @@ void display(Node* root){
     }
     
 
-    while(root!=NULL){
-        printf("%d -> ",root->x);
+    while (root!=NULL){
+        printf("%d -> ",root->x );
         root = root->next;
 
         if(root==NULL){
             printf("NULL");
+            return;
         }
-    }    
+    }
+
+}
+
+void insertEnd (Node** root, int value){
+
+    Node* new_node = malloc(sizeof(Node));
+    new_node->x= value;
+    new_node->next = NULL;  
+
+    if(*root == NULL){
+
+        *root = new_node;
+
+        return;
+    }
+
+    Node* curr = *root;
+    while (curr->next!=NULL)
+    {
+        curr = curr->next;
+    }
+    
+    curr->next = new_node;
+    
 
 }
 
@@ -55,12 +57,12 @@ int main(){
 
     Node* root=NULL;
     insertEnd(&root,24);
-    insertEnd(&root,69);
-    insertEnd(&root,239);
-    insertEnd(&root,2324);
+    insertEnd(&root,245);
+    insertEnd(&root,298);
 
 
     display(root);
+
 
 
     return 0;

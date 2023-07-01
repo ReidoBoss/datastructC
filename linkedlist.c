609d35e0992b48202f39a77647297306887d6681
ReidoBoss <bryansagarino222@gmail.com>
2023-07-02 00:55:41 +0800

    I can finally make linkedlist with my eyes closed

--- linkedlist.c
+++ linkedlist.c
@@ -1,243 +1,272 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 
-typedef struct Node
-{
+typedef struct Node{
     int x;
     struct Node* next;
-
 }Node;
 void display(Node** root){
+
     Node* curr = *root;
     if(curr==NULL){
-        printf("NULL");
+        printf("NULL\n");
         return;
     }
-
-    while(root!=NULL){
-        if(curr->next==NULL)
-        {
-            printf("%d",curr->x);
+    while(curr!=NULL){
+        if(curr->next==NULL){
+            printf("%d\n",curr->x);
             return;
         }
         printf("%d -> ",curr->x);
         curr = curr->next;
     }
-
-
 }
 
-int displayBefore(Node** root,char name[],int print){
+void before(Node** root){
     Node* list = *root;
-    char dash[] = "------------------------";
-
-    int sizeDash = strlen(dash)*2;  
-    int sizeName = strlen(name);
-    int total = sizeDash+sizeName;
-
-    if(print){
-        printf("%s%s%s\nBefore: ", dash,name,dash);
-        display(&list);
-    }
-
-    return total;
-
+    printf("Before:\t ");
+    display(&list);
+    
 }
-void displayAfteR(Node** root,int count){
+void after(Node** root){
+    Node* list = *root;
+    printf("After:\t ");
+    display(&list);
+}
+
+int listSize(Node** root){
     Node* curr = *root;
-    char character = '-';
-    char duplicated[count + 1];
-
-    memset(duplicated, character, count);
-    duplicated[count] = '\0';
-    printf("\nAfter:  ");
-    display(&curr);
-    printf("\n%s\n", duplicated);
-}
-
-void insertEnd(Node** list,int value){
-
-    Node* new_node= malloc(sizeof(Node));
-    new_node->x = value;
-    new_node->next = NULL;
-
-    if(*list==NULL){
-        *list = new_node;
-        return;
-    }
-
-    Node* curr = *list;
-    while(curr->next!=NULL){
+    int i= 0;
+    while(curr!=NULL){
+        i++;
         curr = curr->next;
     }
-    curr->next = new_node;
-
+    return i;
 }
-
-int listSize(Node**root){
-    if(*root==NULL){
-        return 0;
-    }
-    int i = 0;
+int findIndex(Node** root, int value){
     Node* curr = *root;
+    int i=0;
     while (curr!=NULL)
-    {
-        i+=1;
-        curr= curr->next;
-    }   
+    {   
 
-    return i;        
-    
+        i++;
+        if(curr->x == value){
+            return i;
+        }
+        curr = curr->next;
+
+    }
+    return 0;
 }
 
 void sortAsc(Node** root){
-    char name[] = "Ascending Bubble Sort";
-    Node * list = *root;
+    Node* list = *root;
     int size = listSize(&list);
-    displayBefore(&list,name,1);
-
-    for(int i = 1; i < size ; i++){
+    int temp;
+    printf("Ascending Sort:\n");
+    before(&list);
+    for (int i = 1;i <size ;i++){
         Node* curr = *root;
-
         while(curr!=NULL){
-            if(curr->next!=NULL&&curr!=NULL){
+            if(curr->next==NULL){
+
+            }
+            else{
                 if(curr->x > curr->next->x){
+                    temp = curr->x;
+                    curr->x = curr->next->x;
+                    curr->next->x = temp;
+                }
+            }
+            curr= curr->next;
+        }
+    }
+    after(&list);
+}
+void sortDesc(Node** root){
+    printf("Descending Sort:\n");
+    Node* list = *root;
+    int size = listSize(&list);
+    before(&list);
+    for(int i = 0; i < size; i ++){
+        Node* curr = *root;
+        while(curr!=NULL){
+            if(curr->next==NULL){                
+            }
+            else{
+                if(curr->x < curr->next->x){
                     int temp;
                     temp = curr->x;
                     curr->x = curr->next->x;
                     curr->next->x = temp;
                 }
             }
-
-            curr= curr->next;
-        }
-    }
-    int sizeStr = displayBefore(&list,name,0);
-
-    displayAfteR(&list,sizeStr);
-
-
-
-}
-
-void sortDesc(Node** root){
-    Node* list = *root;
-    int size = listSize(&list);
-
-    
-    displayBefore(&list,"Descending Bubble Sort",1);
-    int sizeStr = displayBefore(&list,"Descending Bubble Sort",0);
-
-    for(int i = 1; i < size; i ++ ){
-        Node* curr = *root;
-
-        while(curr!=NULL){
-            if(curr->next!=NULL){
-                if(curr->x < curr->next->x){
-                    int temp;
-                    if(curr->x < curr->next->x){
-                        temp = curr->x;
-                        curr->x = curr->next->x;
-                        curr->next->x = temp;
-                    }    
-                    
-                }
-            }
             curr = curr->next;
         }
+    }
+    after(&list);
+}
 
+void deleteFirst(Node** root){
+    Node* list = *root;
+    if(list ==NULL){
+        printf("Nothing to delete\n");
+        return;
     }
 
-    displayAfteR(&list,sizeStr);
+    *root = list->next;
+}
+void deleteEnd(Node** root){
+    Node* list = *root;
+    printf("Delete End:\n");
+    before(&list);
+    if(list==NULL){
+        printf("nothing to delete\n");
+        return;
+    }
+    while(list!=NULL){
+        if(list->next->next==NULL){
+            list->next=NULL;
 
-
+            Node* curr = *root;
+            after(&curr);
+            return;
+        }
+        list = list->next;
+    }
 
 }
 
-void insertFirst(Node** root,int value){
-
+void insertEnd(Node**root, int value){
     Node* new_node = malloc(sizeof(Node));
-    new_node ->x = value;
+    new_node->x = value;
+    new_node->next = NULL;
+    
+    if(*root==NULL){
+        *root = new_node;
+        return;
+    }
+
+    Node* curr = *root;
+    while(curr->next!=NULL){
+        curr = curr->next;
+    }
+    curr->next = new_node;
+
+}
+void insertFirst(Node** root, int value){
+    Node* new_node = malloc(sizeof(Node));
+    new_node->x = value;
     new_node->next = *root;
 
     *root = new_node;
 }
 
-int findIndex(Node** root, int index){
+void removeElement(Node** root, int value){
     Node* curr = *root;
-    int i = 0;
+    Node* prev = NULL;
+
     while(curr!=NULL){
-        i++;
 
-        if(curr->x==index){
-            printf("\nIndex is at: %d",i);
-            return i;
-        }
-        curr= curr->next;
-
-
-    }
-
-    printf("\nNo index is found");
-    return -1;
-
-}
-
-void deleteFirst(Node**root){
-    if(*root==NULL){
-        printf("Nothing to delete");
-        return;
-    }
-
-    Node* curr = *root;
-    *root = curr->next;
-
-
-}
-
-void deleteEnd(Node**root){
-    Node* curr = *root;
-    if(*root==NULL){
-        printf("Nothing to delete\n");
-        return;
-    }
-    if(curr->next==NULL){
-        *root=NULL;
-        return;
-    }
-    while (curr!=NULL)
-    {
-        if(curr->next->next==NULL){
-            curr->next=NULL;
+        if(curr->x == value){
+            if (prev==NULL)
+            {
+                *root = curr->next;
+                return;
+            }
+            prev->next = curr->next;
             return;
         }
-        curr= curr->next;
+
+        prev = curr;
+        curr = curr->next;
     }
     
- 
+
+}
+
+void addAfter(Node**root, int current ,int value){
+    Node* new_node = malloc(sizeof(Node));
+    new_node->x = value;
+    
+    Node* curr = *root;
+    while (curr!=NULL)
+    {
+        if(curr->x == current){
+            new_node->next = curr->next;
+            curr->next = new_node;
+            return;
+        }
+        curr = curr->next;
+    }
+    
+   printf("Nothing to be replaced\n"); 
+}
+void addBefore(Node** root, int current, int value){
+    Node* new_node = malloc(sizeof(Node));
+    new_node ->x = value;
+
+    Node* curr = *root;
+    Node* prev = NULL;
+    while(curr!=NULL){
+        if(current == curr->x){
+            if(prev==NULL){
+                new_node->next = *root;
+                return;
+            }
+            new_node->next = prev->next;
+            prev->next = new_node;
+        }
+        prev= curr;
+        curr = curr->next;
+    }
+}
+
+void reverse(Node**root){
+    Node* list = *root;
+    int size = listSize(&list);
+    int arr[size];
+    printf("Reverse List: \n");
+    before(&list);
+    
+    for(int i = 0 ; i < size; i ++){
+        arr[i]= list ->x;
+        list = list->next;
+    }
+
+    Node* curr = *root;
+    int i = size-1;
+    while (curr!=NULL)
+    {   
+        curr->x = arr[i];
+        i--;
+        curr=curr->next;
+    }
+    
+    Node* display = *root;
+    after(&display);
+
 }
 
 
 
 
 int main(){
+
     Node* list = NULL;
-    insertEnd(&list, 26);
-    insertEnd(&list, 49);
-    insertEnd(&list, 72);
-    insertEnd(&list, 3);
-    insertEnd(&list, 87);
-    insertEnd(&list, 11);
-    insertEnd(&list, 65);
-    insertEnd(&list, 38);
-    insertEnd(&list, 92);
-    insertEnd(&list, 5);
-    int size = listSize(&list);   
 
+    insertEnd(&list,1);
+    insertEnd(&list,2);
+    insertEnd(&list,3);
+    insertEnd(&list,4);
+    insertEnd(&list,5);
+    insertEnd(&list,6);
+    insertEnd(&list,7);
+    insertEnd(&list,8);
+    display(&list);
 
-    sortDesc(&list);
-    
+    addBefore(&list,4,32);
+    display(&list);
 
     return 0;
 }
